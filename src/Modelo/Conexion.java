b070fb7b216ddeb9489e65f17e8884b11103ccda
shinrak <daremonemo@gmail.com>
2023-05-04 00:18:14 -0500

    primer commit

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,23 @@
+
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+     public static final String url ="jdbc:mysql://127.0.0.1:3306/perusacbd?autoReconnet=true&useSSL=false";
+    public static final String usuario = "root";
+    public static final String contraseña ="1234";
+    
+    Connection conexion=null;
+    
+    public Connection getConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, contraseña);
+        } catch (Exception e) {
+            System.err.println("Error, "+e);
+        }
+        return conexion;
+    }
+}
